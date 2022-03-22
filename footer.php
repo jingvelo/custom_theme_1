e8ebdde797924cc12e100ab7b387c7d83423a1a1
Joni Malaluan <jerniclou@gmail.com>
2022-03-22 12:22:26 +0800

    added html to header and footer

--- footer.php
+++ footer.php
@@ -1,1 +1,103 @@
-Footer here
+<footer class="footer">
+		    	<div class="container">
+		    		<div class="row">
+		    			<div class="col-md-4 pr-30">
+		    				<div class="footer_about">
+		    					<h3>ABOUT US</h3>
+		    					<div class="line-title-left"></div>
+		    					<p class="">Lorem ipsum dolor sit amet, eu me seo  laboramus, iudico dummy  text <br>vituperatoribus at usu cum ex nostrud singulis, vis et nibh</p>
+		    				</div>
+		    				<div class="footer_subscribe">
+		    					<h5>SUBCRIBE NOW</h5>
+								<div class="input-group">
+						            <input type="text" class="form-control" placeholder="Enter your email here">
+						            <span class="input-group-btn">
+						                <button type="submit"><i class="fa fa-paper-plane"></i></button>
+						            </span>
+						        </div>
+		    				</div>
+		    			</div>
+		    			<div class="col-md-4">
+		    				<div class="latest_posts">
+		    					<h3>LATEST POSTS</h3>
+		    					<div class="line-title-left"></div>
+		    					<ul class="post-link">
+									<li>
+										<a href="#">If you need a crown or <br>lorem an implant you will pay it gap</a>
+										<br>
+										<span>July 2, 2014</span>
+									</li>
+									<li>
+										<a href="#">If you need a crown or <br>lorem an implant you will pay it gap</a>
+										<br>
+										<span>July 2, 2014</span>
+									</li>
+								</ul>
+		    				</div>
+		    			</div>
+		    			<div class="col-md-4">
+		    				<div class="footer_address">
+		    					<h3>ADDRESS</h3>
+		    					<div class="line-title-left"></div>
+		    					<ul class="address-list">
+									<li>
+										<p><i class="fas fa-map-marker"></i>60 Grant Ave. Carteret NJ 0708</p>
+									</li>
+									<li>
+										<p><i class="fas fa-mobile-alt"></i>(880) 1723801729</p>
+									</li>
+									<li>
+										<p><i class="far fa-envelope"></i>example@gmail.com</p>
+									</li>
+								</ul>
+								<ul class="footer_social list-inline">
+									<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									<li class="list-inline-item"><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+								</ul>
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    	<div class="sub-footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<p class="text-center">
+									Theme Created By <a href="#">Edify.</a> All Rights Reserved.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+		    </footer>
+
+
+
+		    
+
+			
+	</div>
+	 <!-- ***** End Main Wrapper ***** -->
+
+
+	<!-- Jquery-2.2.4 JS -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- login JS -->
+    <script src="js/login.js"></script>
+    <!-- Bootstrap-4 Beta JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!--Jquery Easing Js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+	<!--Slick Js -->
+	<script src="js/slick.min.js"></script>
+	<!--Magnific Popup Js -->
+	<script src="js/magnific-popup.js"></script>
+	<!--OWL Carousel Js -->
+	<script src="js/owl-carousel.min.js"></script>	
+    <!-- Custom JS -->
+    <script src="js/custom.js"></script>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,91 @@
-Header here
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Fitness HTML Template">
+	<meta name="author" content="CodeGlamour">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Edify - Home 2 Multipurpose bootstrap 4 Html Template</title>
+	<!--Favicon-->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <!--Google Font-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+    <!-- Bootstrap Css-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- Icons Css-->
+    <link rel="stylesheet" type="text/css" href="css/themify-icons.css">
+    <!-- Font Awesome Css-->
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+    <!-- OWL Carousel Css-->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <!-- Slick Css -->
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <!-- Magnific Popup Css -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+    <!-- Custom Css -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<!-- Preloader -->
+	<div id="preloader">
+	  <div class="ozient-folding-cube">
+		  <div class="ozient-cube1 ozient-cube"></div>
+		  <div class="ozient-cube2 ozient-cube"></div>
+		  <div class="ozient-cube4 ozient-cube"></div>
+		  <div class="ozient-cube3 ozient-cube"></div>
+		</div>
+	</div>
+	<!-- *****Main Wrapper***** -->
+	<div id="home" class="main-wrapper">
+		<!-- SUB NAVBAR -->
+		<div class="top_navbar d-none d-lg-block">
+			<nav class="navbar">
+		        <div class="container">
+		          <span class="color-yellow d-none d-lg-block fa fa-map-marker"> <a class="text-white" href="">&nbsp;101, Avenue, New York, NY 10018 US.</a></span> 
+		&emsp;&emsp; <span class="color-yellow d-none d-lg-block fa fa-clock"><a class="text-white" href="">&nbsp; 212 396 5575,212 524 458</a></span>
+		              <span class="color-yellow d-none d-lg-block fa fa-clock ml-auto"><a class="text-white" href="">&nbsp;&nbsp;Mon-Sat, 8.00-18.00. Sunday CLOSED</a></span>
+		          </div>
+		    </nav>
+		</div>
+		
+		<header class="main-header">
+			<div class="main_navbar">	
+			<!-- MAIN NAVBAR -->
+						<nav class="navbar navbar-expand-lg navbar-dark">
+					<div class="container">
+					  <a class="navbar-brand logo-sticky" href="index.html"><img class="img-fluid" src="images/bg-img/logo.png" alt="Edify" /></a>
+					  <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+						<div class="collapse navbar-collapse" id="navbarNav">
+						  <ul class="navbar-nav mr-auto">
+							 <li class="nav-item">
+									<a href="/" class="nav-link">Home</a>
+								  </li>
+								  <li class="nav-item">
+									<a href="about.html" class="nav-link">About</a>
+								  </li>
+								  <li class="nav-item">
+									<a href="services.html" class="nav-link">Services</a>
+								  </li>
+								   <li class="nav-item">
+									<a href="blog_page.html" class="nav-link">Blog</a>
+								  </li>
+								  
+								  <li class="nav-item">
+									<a href="contact.html" class="nav-link">Contact</a>
+								  </li>
+						  </ul>
+						  
+						  <ul class="navbar-nav ml-auto">
+								   <li class="nav-item">
+									  <a class="link-outline" href="#"><i class="icon fas fa-sign-in-alt"></i>&nbsp;Book now</a>
+								  </li>
+							  </ul>
+						</div>
+					  </div>
+				</nav>
+		    </div>
+		</header>
+
